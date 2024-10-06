20fc5433865a6364513fbfbdca6fc66ae6f3042e
WaqaraSadhayo <waqarahmed786462003@gmail.com>
2024-10-06 13:46:14 +0500

    First push of this file

---
+++ src/app/responsive/page.tsx
@@ -0,0 +1,35 @@
+export default function Res() {
+  return (
+    <div className="parent bounce-in-right">
+      <div className="child">
+        <div className="imagecontainer"></div>
+        <h1 className="title">Blog Title</h1>
+        <p className="description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          repudiandae!
+        </p>
+        <button className="readmore">Read more</button>
+      </div>
+
+      <div className="child">
+        <div className="imagecontainer"></div>
+        <h1 className="title">Blog Title</h1>
+        <p className="description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          repudiandae!
+        </p>
+        <button className="readmore">Read more</button>
+      </div>
+
+      <div className="child">
+        <div className="imagecontainer"></div>
+        <h1 className="title">Blog Title</h1>
+        <p className="description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima,
+          repudiandae!
+        </p>
+        <button className="readmore">Read more</button>
+      </div>
+    </div>
+  );
+}
